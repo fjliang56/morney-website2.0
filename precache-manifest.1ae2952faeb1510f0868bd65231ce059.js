5241b4791ba09a53bcda7436a2371cf819970fba
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-10 20:44:41 +0800

    deploy

---
+++ precache-manifest.1ae2952faeb1510f0868bd65231ce059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a5ef71953c984acfe1",
+    "url": "/morney-website2.0/css/app.4e2a3d9d.css"
+  },
+  {
+    "revision": "b4119dc85d17abc853662b4178669143",
+    "url": "/morney-website2.0/index.html"
+  },
+  {
+    "revision": "17a5ef71953c984acfe1",
+    "url": "/morney-website2.0/js/app.d8667433.js"
+  },
+  {
+    "revision": "f13363b273321276fd41",
+    "url": "/morney-website2.0/js/chunk-vendors.a63ce8bd.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website2.0/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website2.0/robots.txt"
+  }
+]);
